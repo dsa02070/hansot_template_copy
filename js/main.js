560fc6d93d9af9d6fbb0b9cf70f08a2375494876
dsa02070 <82920762+dsa02070@users.noreply.github.com>
2021-04-26 22:26:42 +0900

    update all
    html, js, css 업데이트
    탑 버튼 추가, section 효과 추가, 미디어 쿼리 어색한 부분 추가, 메인 배너 텍스트 효과 추가

--- js/main.js
+++ js/main.js
@@ -3,10 +3,46 @@
 function gnbControl() {
     document.querySelector("#gnb_btn").addEventListener('click', function () {
         document.querySelector("#mgnb").style.left = "0%"
-    })
+    });
     document.getElementById("mgnb_btn").addEventListener('click', function () {
         document.getElementById("mgnb").style.left = "-100%"
-    })
-}
+    });
+};
 gnbControl();//함수호출
 
+// section #third, #fourth, #fifth 등장 효과
+// 태그의 높이값 구하기
+let thirdTop = document.querySelector('#third').getBoundingClientRect().top;
+let fourthTop = document.querySelector('#fourth').getBoundingClientRect().top;
+let fifthTop = document.querySelector('#fifth').getBoundingClientRect().top;
+
+// 태그 선택 변수 처리하기
+let third = document.querySelector('#third');
+let fourth = document.querySelector('#fourth');
+let fifth = document.querySelector('#fifth');
+
+//화면 내릴 때 하나씩 등장
+function scrollEvent() {
+    if (document.documentElement.scrollTop >= thirdTop - 600) {
+        third.style.opacity = '100%';
+    };
+    if (document.documentElement.scrollTop >= fourthTop - 600) {
+        fourth.style.opacity = '100%';
+    };
+    if (document.documentElement.scrollTop >= fifthTop - 600) {
+        fifth.style.opacity = '100%';
+    };
+};
+
+// .top 버튼 누르면 상단으로
+function goTop() {
+    if (document.documentElement.scrollTop >= 50) { // 스크롤탑이 150 이상되면
+        document.querySelector('.top').style.right = '15px' // right : 15px
+    } else {
+        document.querySelector('.top').style.right = '-50px' // 150 미만이라면 -50px
+    };
+    document.querySelector('.top').addEventListener('click', function () {
+        window.scroll({ top: 0, behavior: 'smooth' }); // 버튼을 클릭하면 top 0으로 부드럽게 이동
+    });
+};
+window.onscroll = function () { goTop(), scrollEvent() };
